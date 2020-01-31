63f829c38193b45879c92459e6e459829ae3c7d0
dan.wttvn <dan.wttvn@gmail.com>
2020-01-31 13:55:54 +0100

    party loyalty FIN

--- js/statics_PL.js
+++ js/statics_PL.js
@@ -1,12 +1,10 @@
-//SENATE VARIABLES
+// ** SOLO HE TENIDO QUE CAMBIAR votes_with_party_pct y el signo <
+
 let membersData = data.results[0].members;
 
 // CALLING
 // createGlobalTable(membersData);
-createEngagementTables(membersData, 10);
-
-
-
+createEngagementTables(membersData, 10); //se ponen al reves?
 
 
 // ! no consigo que me funcione en funcion 
@@ -34,9 +32,6 @@ createEngagementTables(membersData, 10);
 	displayGlanceTable(statics);
 
 // }
-
-
-
 
 
 // ******* GLOBAL GLANCE *****
@@ -90,24 +85,24 @@ function displayGlanceTable(staticsArray) {
 }
 
 
-// ********************* ENGAGEMENT ****************
+// ********************* LOYALTY ****************
 
 function createEngagementTables(membersArray, percentage) {
 	sortObjectByValue(membersArray);
 
-	let mostEngagementTable = document.getElementById("mostEngagementTable");
-	displayDataIntoTable_Attendance(membersArray, percentage, mostEngagementTable);
+	let mostTable = document.getElementById("mostTable");
+	displayDataIntoTable_Attendance(membersArray, percentage, mostTable);
 
-	let lessEngagementTable = document.getElementById("lessEngagementTable");
+	let leastTable = document.getElementById("leastTable");
 	membersArray.reverse();
-	displayDataIntoTable_Attendance(membersArray, percentage, lessEngagementTable);
+	displayDataIntoTable_Attendance(membersArray, percentage, leastTable);
 
 }
 
 // ------ SORT OBJ MEMBERS BY ENGAGEMENT
 function sortObjectByValue(membersArray) {
 	membersArray.sort((a, b) => {
-		if (a.missed_votes_pct > b.missed_votes_pct) {
+		if (a.votes_with_party_pct < b.votes_with_party_pct) { //!! HE TENIDO QUE CAMBIAR < ççççç
 			return 1;
 		} else {
 			return -1;
@@ -131,7 +126,7 @@ function displayDataIntoTable_Attendance(membersArray, percentage, tableToDispla
 			fullName = membersArray[i].first_name + " " + membersArray[i].middle_name + " " + membersArray[i].last_name; 
 		}
 		missedVotes = membersArray[i].missed_votes; 
-		engagement = membersArray[i].missed_votes_pct; 
+		engagement = membersArray[i].votes_with_party_pct; 
 		if (membersArray[i].middle_name === null) {
 			wikiURL = "https://en.wikipedia.org/wiki/" + membersArray[i].first_name + "_" + membersArray[i].last_name;
 		} else {
@@ -157,37 +152,3 @@ function displayDataIntoTable_Attendance(membersArray, percentage, tableToDispla
 	}
 }
 
-// FUNCIONES DENTRO DE BUCLE
-function getDataForEngagement(membersArray) { // says [i] is not define. como funcion dentro de bucle?
-// 	if (membersData[i].middle_name === null) {
-// 		fullName = membersData[i].first_name + " " + membersData[i].last_name;
-// 	} else {
-// 		fullName = membersData[i].first_name + " " + membersData[i].middle_name + " " + membersData[i].last_name; 
-// 	}
-// 	missedVotes = membersData[i].missed_votes; 
-// 	engagement = membersData[i].missed_votes_pct; 
-// 	if (membersData[i].middle_name === null) {
-// 		wikiURL = "https://en.wikipedia.org/wiki/" + membersData[i].first_name + "_" + membersData[i].last_name;
-// 	} else {
-// 		wikiURL = "https://en.wikipedia.org/wiki/" + membersData[i].first_name + "_" + membersData[i].middle_name + "_" + membersData[i].last_name; 
-// 	}
-}
-
-function displayDataIntoTable(membersArray) {
-	// let newRow = document.createElement("tr");
-
-	// let td1 = document.createElement("td");
-	// let linkTag = document.createElement("a");
-	// td1.appendChild(linkTag);
-	// newRow.appendChild(td1);
-	// td1.setAttribute("class", "alignLeft");
-	// linkTag.setAttribute("href", wikiURL);
-	// linkTag.innerHTML = fullName;
-
-	// let td2 = newRow.appendChild(document.createElement("td"));
-	// td2.innerHTML = missed_votes;
-	// let td3 = newRow.appendChild(document.createElement("td"));
-	// td3.innerHTML = engagement;
-
-	// mostEngagementTable.appendChild(newRow);
-}
